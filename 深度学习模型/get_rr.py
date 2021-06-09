215f9e97470fcf3593384c81df8ef5a204e2d256
sjchasel <1296841437@qq.com>
2021-06-09 23:34:42 +0800

    cnn1D等

--- 深度学习模型/get_rr.py
+++ 深度学习模型/get_rr.py
@@ -1,48 +1,15 @@
-def __sst(y_no_fitting):
-    """
-    计算SST(total sum of squares) 总平方和
-    :param y_no_predicted: List[int] or array[int] 待拟合的y
-    :return: 总平方和SST
-    """
-    y_mean = sum(y_no_fitting) / len(y_no_fitting)
-    s_list =[(y - y_mean)**2 for y in y_no_fitting]
-    sst = sum(s_list)
-    return sst
-
-
-def __ssr(y_fitting, y_no_fitting):
-    """
-    计算SSR(regression sum of squares) 回归平方和
-    :param y_fitting: List[int] or array[int]  拟合好的y值
-    :param y_no_fitting: List[int] or array[int] 待拟合y值
-    :return: 回归平方和SSR
-    """
-    y_mean = sum(y_no_fitting) / len(y_no_fitting)
-    s_list =[(y - y_mean)**2 for y in y_fitting]
-    ssr = sum(s_list)
-    return ssr
-
-
-def __sse(y_fitting, y_no_fitting):
-    """
-    计算SSE(error sum of squares) 残差平方和
-    :param y_fitting: List[int] or array[int] 拟合好的y值
-    :param y_no_fitting: List[int] or array[int] 待拟合y值
-    :return: 残差平方和SSE
-    """
-    s_list = [(y_fitting[i] - y_no_fitting[i])**2 for i in range(len(y_fitting))]
-    sse = sum(s_list)
-    return sse
-
-
-def goodness_of_fit(y_fitting, y_no_fitting):
-    """
-    计算拟合优度R^2
-    :param y_fitting: List[int] or array[int] 拟合好的y值
-    :param y_no_fitting: List[int] or array[int] 待拟合y值
-    :return: 拟合优度R^2
-    """
-    SSR = __ssr(y_fitting, y_no_fitting)
-    SST = __sst(y_no_fitting)
-    rr = SSR /SST
-    return rr
+def goodness_of_fit(X, Y):
+    xBar = np.mean(X)
+    yBar = np.mean(Y)
+    SSR = 0
+    varX = 0
+    varY = 0
+    for i in range(0 , len(X)):
+        diffXXBar = X[i] - xBar
+        diffYYBar = Y[i] - yBar
+        SSR += (diffXXBar * diffYYBar)
+        varX +=  diffXXBar**2
+        varY += diffYYBar**2
+    
+    SST = math.sqrt(varX * varY)
+    return (SSR / SST)**2
